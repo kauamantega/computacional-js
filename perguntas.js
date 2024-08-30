87dc5a17ac386c87c65f2d801f2344ca2f9dcc57
kauamantega <167893166+kauamantega@users.noreply.github.com>
2024-08-30 16:43:47 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Quantos litros de água, em média, uma pessoa deve beber por dia para se manter hidratada?",
+        alternativas: [
+            {
+                texto: "A) 3 litros",
+                afirmacao: [
+                    "b) 2 litros"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "b) 2 litros",
+                afirmacao: [
+                    "b) 2 litros"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o nutriente mais importante para a construção e reparo dos músculos?",
+        alternativas: [
+            {
+                texto: "a) Carboidrato",
+                afirmacao: [
+                    "B)Proteína",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B)Proteína",
+                afirmacao: [
+                    "B) Proteína"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a quantidade mínima de atividade física recomendada pela OMS para adultos por semana??",
+        alternativas: [
+            {
+                texto: "a) 75 minutos",
+                afirmacao: [
+                    "b) 150 minutos"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) 150 minutos",
+                afirmacao: [
+                    "b) 150 minutos"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual desses alimentos é rico em antioxidantes e ajuda a combater os radicais livres no corpo?",
+        alternativas: [
+            {
+                texto: "A)Frutas vermelhas",
+                afirmacao: [
+                    "A)Frutas vermelhas"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) Laranja",
+                afirmacao: [
+                    "A)Frutas vermelhas"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "O que significa o termo 'IMC'?",
+        alternativas: [
+            {
+                texto: "a) Índice de Massa Corporal",
+                afirmacao: [
+                    "a) Índice de Massa Corporal"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) Índice Metabólico Cardíaco",
+                afirmacao: [
+                    "a) Índice de Massa Corporal"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "O que o sedentarismo pode causar a longo prazo?",
+        alternativas: [
+            {
+                texto: "a) Perda de fexibilidade",
+                afirmacao: [
+                    "b) Perda de massa muscular"
+                ],
+            },
+            {
+                texto: "b) Perda de massa muscular",
+                afirmacao: [
+                    "b) Perda de massa muscular"
+                ],
+            },
+        ]
+    },
+];
